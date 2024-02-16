6fe9b69b871537d25c9929fef129976d55b0902f
ArsenHandzhyan <arsenhan@internet.ru>
2024-02-16 06:15:23 +0300

    fix UrlHelper

--- app/src/main/java/hexlet/code/repository/UrlHelper.java
+++ app/src/main/java/hexlet/code/repository/UrlHelper.java
@@ -7,22 +7,32 @@ public class UrlHelper {
     public static String normalizeUrl(String inputUrl) {
         try {
             URI uri = new URI(inputUrl);
-            String scheme = uri.getScheme();
-            String host = uri.getHost();
-            if (scheme == null || (!scheme.equalsIgnoreCase("http") && !scheme.equalsIgnoreCase("https"))) {
-                return null; // Некорректная схема URL
-            }
-            if (host == null) {
-                return null; // Отсутствует хост в URL
+            String scheme = getScheme(uri);
+            String host = getHost(uri);
+            if (scheme == null || host == null) {
+                return null;
             }
             int port = uri.getPort();
-            if (port == -1) {
-                return scheme + "://" + host;
-            } else {
-                return scheme + "://" + host + ":" + port;
-            }
+            return formatNormalizedUrl(scheme, host, port);
         } catch (URISyntaxException e) {
-            return null; // Некорректный URL
+            return null;
+        }
+    }
+
+    private static String getScheme(URI uri) {
+        String scheme = uri.getScheme();
+        return (scheme != null && (scheme.equalsIgnoreCase("http") || scheme.equalsIgnoreCase("https"))) ? scheme : null;
+    }
+
+    private static String getHost(URI uri) {
+        return uri.getHost();
+    }
+
+    private static String formatNormalizedUrl(String scheme, String host, int port) {
+        if (port == -1) {
+            return scheme + "://" + host;
+        } else {
+            return scheme + "://" + host + ":" + port;
         }
     }
 }
