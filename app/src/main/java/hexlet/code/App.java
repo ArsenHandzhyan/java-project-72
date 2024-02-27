07b8e655f7aa21511a60f742d17bda3531d925e3
ArsenHandzhyan <arsenhan@internet.ru>
2024-02-27 20:31:10 +0300

    fix App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -49,9 +49,11 @@ public class App {
 
     private static void configureRoutes() {
         app.get(NamedRoutes.homePath(), MainController::index);
+        app.post(NamedRoutes.homePath(), MainController::addUrl);
         app.get(NamedRoutes.urlsPath(), UrlsController::showAllUrls);
-        app.post(NamedRoutes.urlsPath(), MainController::addUrl);
+        app.post(NamedRoutes.urlsPath(), UrlsController::showAllUrls);
         app.get(NamedRoutes.urlPath("{id}"), UrlsController::showUrlById);
+        app.get(NamedRoutes.checksUrlPath("{id}"), UrlsController::checkUrl);
         app.post(NamedRoutes.checksUrlPath("{id}"), UrlsController::checkUrl);
     }
 
