a94d59f49dfef62b605a658b623474a84dff9aff
arsen <arsenhan@internet.ru>
2024-04-08 21:50:37 +0300

    fix AppTest

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,7 +1,6 @@
 package hexlet.code;
 
 import hexlet.code.model.Url;
-import hexlet.code.repository.UrlCheckRepository;
 import hexlet.code.repository.UrlsRepository;
 import hexlet.code.util.NamedRoutes;
 import io.javalin.Javalin;
