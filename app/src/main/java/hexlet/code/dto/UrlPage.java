0a8b2ffa89697ccf79556e5da72ef471ba970a5d
arsen <arsenhan@internet.ru>
2024-04-12 03:39:01 +0300

    fix UrlPage

--- app/src/main/java/hexlet/code/dto/UrlPage.java
+++ app/src/main/java/hexlet/code/dto/UrlPage.java
@@ -13,11 +13,27 @@ public class UrlPage extends BasePage {
     private Url url;
     private List<UrlCheck> urlChecks;
 
+    /**
+     * Returns the flash message.
+     *
+     * <p>This method is intended for use in subclasses. If you override this method, ensure that
+     * you call the superclass's implementation to maintain the correct behavior.</p>
+     *
+     * @return the flash message
+     */
     @Override
     public String getFlash() {
         return super.getFlash();
     }
 
+    /**
+     * Sets the flash message.
+     *
+     * <p>This method is intended for use in subclasses. If you override this method, ensure that
+     * you call the superclass's implementation to maintain the correct behavior.</p>
+     *
+     * @param flash the flash message to set
+     */
     @Override
     public void setFlash(String flash) {
         super.setFlash(flash);
