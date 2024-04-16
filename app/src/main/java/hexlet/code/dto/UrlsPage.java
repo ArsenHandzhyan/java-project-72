d8a7ac1cc77530108ee11039362c5997a575231f
arsen <arsenhan@internet.ru>
2024-04-17 01:51:52 +0300

    fix index.jte

--- app/src/main/java/hexlet/code/dto/UrlsPage.java
+++ app/src/main/java/hexlet/code/dto/UrlsPage.java
@@ -1,13 +1,10 @@
 package hexlet.code.dto;
 
 import hexlet.code.model.Url;
-import hexlet.code.repository.UrlCheckRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * This class represents a page of URLs with additional information about the last check date and status code.
@@ -18,27 +15,6 @@ import java.util.Objects;
 @Getter
 public class UrlsPage extends BasePage {
     private List<Url> urls;
-
-    /**
-     * Retrieves the HTTP status code of the last check for the URL with the specified ID.
-     *
-     * @param id the ID of the URL
-     * @return the HTTP status code as a string, or an empty string if the URL check is not found
-     */
-    public String getStatusCode(Long id) {
-        return String.valueOf(Objects.requireNonNull(UrlCheckRepository.findLastByUrlId(id)).getStatusCode());
-    }
-
-    /**
-     * Retrieves the date and time of the last check for the URL with the specified ID.
-     *
-     * @param id the ID of the URL
-     * @return the date and time of the last check in the format "dd/MM/yyyy HH:mm",
-     * zor an empty string if the URL check is not found
-     */
-    public LocalDateTime getLastCheckDate(Long id) {
-        return Objects.requireNonNull(UrlCheckRepository.findLastByUrlId(id)).getCreatedAt();
-    }
 
     /**
      * Gets the flash message.
--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -1,15 +1,20 @@
 package hexlet.code.model;
 
+import hexlet.code.repository.UrlCheckRepository;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -23,8 +28,32 @@ public class Url {
     private String name;
     private LocalDateTime createdAt;
 
+    @OneToMany(mappedBy = "url", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UrlCheck> urlChecks = new ArrayList<>();
+
     public Url(String name, LocalDateTime createdAt) {
         this.name = name;
         this.createdAt = createdAt;
+    }
+
+    /**
+     * Retrieves the HTTP status code of the last check.
+     *
+     * @return the HTTP status code as a string, or an empty string if the URL check is not found
+     */
+    public String getStatusCode() {
+        UrlCheck lastCheck = UrlCheckRepository.findLastByUrlId(this.id);
+        return lastCheck != null ? String.valueOf(lastCheck.getStatusCode()) : "";
+    }
+
+    /**
+     * Retrieves the date and time of the last check.
+     *
+     * @return the date and time of the last check in the format "dd/MM/yyyy HH:mm",
+     * or an empty string if the URL check is not found
+     */
+    public LocalDateTime getLastCheckDate() {
+        UrlCheck lastCheck = UrlCheckRepository.findLastByUrlId(this.id);
+        return lastCheck != null ? lastCheck.getCreatedAt() : null;
     }
 }
