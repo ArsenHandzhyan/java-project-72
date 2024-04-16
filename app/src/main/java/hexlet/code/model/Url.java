2d3e25fd9dff171367e0cd4ee7aee86ddbf4a0dd
arsen <arsenhan@internet.ru>
2024-04-17 02:57:26 +0300

    fix Url

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -6,15 +6,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -28,19 +24,11 @@ public class Url {
     private String name;
     private LocalDateTime createdAt;
 
-    @OneToMany(mappedBy = "url", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<UrlCheck> urlChecks = new ArrayList<>();
-
     public Url(String name, LocalDateTime createdAt) {
         this.name = name;
         this.createdAt = createdAt;
     }
 
-    /**
-     * Retrieves the HTTP status code of the last check.
-     *
-     * @return the HTTP status code as a string, or an empty string if the URL check is not found
-     */
     public String getStatusCode() {
         UrlCheck lastCheck = UrlCheckRepository.findLastByUrlId(this.id);
         return lastCheck != null ? String.valueOf(lastCheck.getStatusCode()) : "";
