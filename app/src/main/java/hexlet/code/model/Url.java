0041a956328e433ae61b531370950bc170776454
arsen <arsenhan@internet.ru>
2024-04-17 03:04:05 +0300

    fix index.jte

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -1,5 +1,6 @@
 package hexlet.code.model;
 
+import hexlet.code.repository.UrlCheckRepository;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,5 +27,21 @@ public class Url {
     public Url(String name, LocalDateTime createdAt) {
         this.name = name;
         this.createdAt = createdAt;
+    }
+
+    public String getStatusCode() {
+        UrlCheck lastCheck = UrlCheckRepository.findLastByUrlId(this.id);
+        return lastCheck != null ? String.valueOf(lastCheck.getStatusCode()) : "";
+    }
+
+    /**
+     * Retrieves the date and time of the last check.
+     *
+     * @return the date and time of the last check in the format "dd/MM/yyyy HH:mm",
+     * or an empty string if the URL check is not found
+     */
+    public LocalDateTime getLastCheckDate() {
+        UrlCheck lastCheck = UrlCheckRepository.findLastByUrlId(this.id);
+        return lastCheck != null ? lastCheck.getCreatedAt() : null;
     }
 }
