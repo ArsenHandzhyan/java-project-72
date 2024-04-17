b7b78dc9f46911d55ccbcebbc2508c7a9fffabd1
arsen <arsenhan@internet.ru>
2024-04-17 17:12:07 +0300

    fix index.jte

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -1,20 +1,15 @@
 package hexlet.code.model;
 
-import hexlet.code.repository.UrlCheckRepository;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -28,21 +23,8 @@ public class Url {
     private String name;
     private LocalDateTime createdAt;
 
-    @OneToMany(mappedBy = "url", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<UrlCheck> urlChecks = new ArrayList<>();
-
     public Url(String name, LocalDateTime createdAt) {
         this.name = name;
         this.createdAt = createdAt;
-    }
-
-    public String getStatusCode() {
-        UrlCheck lastCheck = UrlCheckRepository.findLastByUrlId(this.id);
-        return lastCheck != null ? String.valueOf(lastCheck.getStatusCode()) : "";
-    }
-
-    public LocalDateTime getLastCheckDate() {
-        UrlCheck lastCheck = UrlCheckRepository.findLastByUrlId(this.id);
-        return lastCheck != null ? lastCheck.getCreatedAt() : null;
     }
 }
